48852a666e70144b3f89b68e3737b68be559a29d
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2022-07-24 14:37:56 -0300

    tests[STOCK]: write tests

--- stock_service/stocks/functions.py
+++ stock_service/stocks/functions.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+import requests
+
 def parse_stock(stock_line):
     stock_split = stock_line.lower().split('\n')
 
@@ -5,5 +8,12 @@ def parse_stock(stock_line):
     values = stock_split[1].replace('\r','').split(',')
 
     stock = dict(zip(keys, values))
+
+    return stock
+
+def get_stock(stock_code):
+    url = settings.STOOQ_URL + f'&s={stock_code}'
+    resp = requests.get(url)
+    stock = parse_stock(resp.text)
 
     return stock
--- stock_service/stocks/views.py
+++ stock_service/stocks/views.py
@@ -1,11 +1,8 @@
 # encoding: utf-8
-import requests, json
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.conf import settings
 from stocks.serializer import StockSerializer
-from stocks.utils import parse_stock
+from stocks.functions import get_stock
 from datetime import datetime
 
 LOG_PREFIX = '[stock][views]'
@@ -18,10 +15,7 @@ class StockView(APIView):
         print(f'{LOG_PREFIX}[stock][GET] request received')
         stock_code = request.query_params.get('stock_code')
         print(f'{LOG_PREFIX}[stock][GET] searching for stock code: {stock_code}')
-        url = settings.STOOQ_URL + f'&s={stock_code}'
-
-        resp = requests.get(url)
-        stock = parse_stock(resp.text)
+        stock = get_stock(stock_code)
 
         stock['date'] = datetime.now()
 
