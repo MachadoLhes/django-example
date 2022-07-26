77b796fa5696c816561ec7328da9c266346b6643
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2022-07-26 12:10:45 -0300

    fix[STOCK]: Round decimlas with more than 2 decimal places

--- stock_service/stocks/serializer.py
+++ stock_service/stocks/serializer.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 
+class RoundingDecimalField(serializers.DecimalField):
+    def validate_precision(self, value):
+        return value
+
 class StockSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     symbol = serializers.CharField(max_length=20)
-    open = serializers.DecimalField(max_digits=10, decimal_places=2)
-    high = serializers.DecimalField(max_digits=10, decimal_places=2)
-    low = serializers.DecimalField(max_digits=10, decimal_places=2)
-    close = serializers.DecimalField(max_digits=10, decimal_places=2)
+    open = RoundingDecimalField(max_digits=10, decimal_places=2)
+    high = RoundingDecimalField(max_digits=10, decimal_places=2)
+    low = RoundingDecimalField(max_digits=10, decimal_places=2)
+    close = RoundingDecimalField(max_digits=10, decimal_places=2)
     date = serializers.DateTimeField()
